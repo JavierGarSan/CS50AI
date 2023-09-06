9ab48a84eb609d5c8394deb4ae62cb198a1c7bf1
JavierGarSan <jgsanmil@outlook.es>
2023-09-06 10:22:54 +0200

    MOre work

--- 0/Tictactoe/tictactoe.py
+++ 0/Tictactoe/tictactoe.py
@@ -54,8 +54,8 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     resultboard = board
-    if action in possible:
-        resultboard[action[0]][action[1]] = currentPlayer
+    if action in actions(board):
+        resultboard[action[0]][action[1]] = player(board)
     else:
         raise Exception("Action is not possible")
 
@@ -67,32 +67,94 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    columns = [[],[],[]]
+    winner = None
+    
+    diagonalLR = []
+    diagonalLR.append(board[0][0])
+    diagonalLR.append(board[1][1])
+    diagonalLR.append(board[2][2])
+    if diagonalLR.count(diagonalLR[0]) == 3 and diagonalLR[0] != EMPTY:
+        winner = diagonalLR[0]
+
+    diagonalRL = []
+    diagonalRL.append(board[0][2])
+    diagonalRL.append(board[1][1])
+    diagonalRL.append(board[2][0])
+
+    if diagonalRL.count(diagonalRL[0]) == 3 and diagonalRL[0] != EMPTY:
+        winner = diagonalRL[0]
+
+    for row in board:
+        if (row.count(row[0]) == 3) and row[0] != EMPTY:
+            winner = row[0] 
+    for row in board:
+        columns[0].append(row[0])
+        columns[1].append(row[1])
+        columns[2].append(row[2])
+    
+    for column in columns:
+        if (column.count(column[0]) == 3) and column[0] != EMPTY:
+            winner = column[0] 
+    
+
+    return winner
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-
-    raise NotImplementedError
+    full = []
+    finished = False
+    for row in board:
+        for item in row:
+            full.append(item)
+    
+    if (full.count(EMPTY)) == 0:
+         finished = True
+    
+    if winner(board) != None:
+        finished= True
+    
+    return finished
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    utility = 0
+    if terminal(board) == True:
+        if winner(board) == X:
+            utility = 1
+        if winner(board) == O:
+            utility = -1
+
+    return utility
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    boards = [board]
+    
+    full = []
+    finished = False
+    for row in board:
+        for item in row:
+            full.append(item)
+    
+    movesleft=full.count(EMPTY)
+    for action in actions(board):
+        print (action)
+        print(result(board, action))
+
+
+    
+    
 
 
 board = initial_state()
-currentPlayer = player(board)
-possible = actions(board)
-print(result(board, (0,0)))
+minimax(board)
